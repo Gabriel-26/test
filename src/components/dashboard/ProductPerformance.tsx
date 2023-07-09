a6c3c505813ae544415723440684fae721c6edf5
Gabriel <gabriel.tejana26@gmail.com>
2023-07-09 22:40:53 +0800

    commit

---
+++ src/components/dashboard/ProductPerformance.tsx
@@ -0,0 +1,230 @@
+import React from "react";
+import {
+  Typography,
+  Box,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Chip,
+  IconButton,
+  TextField,
+} from "@mui/material";
+import DashboardCard from "../../../src/components/shared/DashboardCard";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import DeleteIcon from "@mui/icons-material/Delete";
+import BorderColorIcon from "@mui/icons-material/BorderColor";
+import { green, red } from "@mui/material/colors";
+import SearchIcon from "@mui/icons-material/Search";
+
+const products = [
+  {
+    id: "1",
+    name: "Sunil Joshi",
+    post: "Web Designer",
+    pname: "Elite Admin",
+    priority: "Low",
+    pbg: "primary.main",
+    budget: "3.9",
+  },
+  {
+    id: "2",
+    name: "Andrew McDownland",
+    post: "Project Manager",
+    pname: "Real Homes WP Theme",
+    priority: "Medium",
+    pbg: "secondary.main",
+    budget: "24.5",
+  },
+  {
+    id: "3",
+    name: "Christopher Jamil",
+    post: "Project Manager",
+    pname: "MedicalPro WP Theme",
+    priority: "High",
+    pbg: "error.main",
+    budget: "12.8",
+  },
+  {
+    id: "4",
+    name: "Nirav Joshi",
+    post: "Frontend Engineer",
+    pname: "Hosting Press HTML",
+    priority: "Critical",
+    pbg: "success.main",
+    budget: "2.4",
+  },
+];
+const rows = [
+  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+];
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "firstName", headerName: "First name", width: 130 },
+  { field: "lastName", headerName: "Last name", width: 130 },
+  {
+    field: "age",
+    headerName: "Age",
+    type: "number",
+    width: 90,
+  },
+  {
+    field: "fullName",
+    headerName: "Full name",
+    description: "This column has a value getter and is not sortable.",
+    sortable: false,
+    width: 160,
+    valueGetter: (params: GridValueGetterParams) =>
+      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+  },
+];
+
+const ProductPerformance = () => {
+  return (
+    <DashboardCard title="Doctors">
+      <Box sx={{ overflow: "auto", width: { xs: "280px", sm: "auto" } }}>
+        <TextField
+          label="Search"
+          InputProps={{
+            type: "search",
+          }}
+        />
+        <Table
+          aria-label="simple table"
+          sx={{
+            whiteSpace: "nowrap",
+            mt: 2,
+          }}
+        >
+          {/* <DataGrid
+            sx={{ fontSize: 12, fontWeight: 600, minWidth: 30 }}
+            rows={rows}
+            columns={columns}
+            initialState={{
+              pagination: {
+                paginationModel: { page: 0, pageSize: 5 },
+              },
+            }}
+            pageSizeOptions={[5, 10]}
+            checkboxSelection
+          /> */}
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <Typography variant="subtitle2" fontWeight={600}>
+                  Id
+                </Typography>
+              </TableCell>
+              <TableCell>
+                <Typography variant="subtitle2" fontWeight={600}>
+                  Assigned
+                </Typography>
+              </TableCell>
+              <TableCell>
+                <Typography variant="subtitle2" fontWeight={600}>
+                  Name
+                </Typography>
+              </TableCell>
+              <TableCell>
+                <Typography variant="subtitle2" fontWeight={600}>
+                  Priority
+                </Typography>
+              </TableCell>
+              <TableCell align="right">
+                <Typography variant="subtitle2" fontWeight={600}>
+                  Budget
+                </Typography>
+              </TableCell>
+              <TableCell align="right">
+                <Typography variant="subtitle2" fontWeight={600}>
+                  Actions
+                </Typography>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {products.map((product) => (
+              <TableRow key={product.name}>
+                <TableCell>
+                  <Typography
+                    sx={{
+                      fontSize: "15px",
+                      fontWeight: "500",
+                    }}
+                  >
+                    {product.id}
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Box>
+                      <Typography variant="subtitle2" fontWeight={600}>
+                        {product.name}
+                      </Typography>
+                      <Typography
+                        color="textSecondary"
+                        sx={{
+                          fontSize: "13px",
+                        }}
+                      >
+                        {product.name}
+                      </Typography>
+                    </Box>
+                  </Box>
+                </TableCell>
+                <TableCell>
+                  <Typography
+                    color="textSecondary"
+                    variant="subtitle2"
+                    fontWeight={400}
+                  >
+                    {product.name}
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Chip
+                    sx={{
+                      px: "4px",
+                      backgroundColor: product.pbg,
+                      color: "#fff",
+                    }}
+                    size="small"
+                    label={product.priority}
+                  ></Chip>
+                </TableCell>
+                <TableCell align="right">
+                  <Typography variant="h6">${product.budget}k</Typography>
+                </TableCell>
+                <TableCell align="right">
+                  <IconButton>
+                    <DeleteIcon sx={{ color: green[400] }} />
+                  </IconButton>
+                  <IconButton sx={{ marginX: -1 }}>
+                    <BorderColorIcon sx={{ color: red[400] }} />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Box>
+    </DashboardCard>
+  );
+};
+
+export default ProductPerformance;
---
+++ src/layouts/full/shared/logo/Logo.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import { styled } from "@mui/material";
+import Image from "next/image";
+
+const LinkStyled = styled(Link)(() => ({
+  height: "70px",
+  width: "180px",
+  overflow: "hidden",
+  display: "block",
+}));
+
+const Logo = () => {
+  return (
+    <LinkStyled href="/">
+      <Image
+        src="/images/logos/logo.png"
+        alt="logo"
+        height={80}
+        width={150}
+        priority
+      />
+    </LinkStyled>
+  );
+};
+
+export default Logo;
