128d68d2b4b8f8c5a7a1c393df7b6c859209c203
Gabriel <gabriel.tejana26@gmail.com>
2024-02-04 19:31:54 +0800

    ui changes

--- src/layouts/full/header/Profile.tsx
+++ src/layouts/full/header/Profile.tsx
@@ -1,18 +1,29 @@
 import React, { useEffect, useState } from "react";
-import { Avatar, Box, Menu, Button, IconButton, MenuItem } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Menu,
+  Button,
+  IconButton,
+  MenuItem,
+  Typography,
+} from "@mui/material";
 import { IconUser } from "@tabler/icons-react";
 import useAdminAuth from "../../../components/utils/useAdminAuth";
 import useResidentAuth from "../../../components/utils/useResidentAuth";
 
-// Import the image
+// Import the images
 import doctorMaleImage from "../../../assets/images/profile/doctorm.png";
 import doctorFemaleImage from "../../../assets/images/profile/femaledoc.png";
+import adminImage from "../../../assets/images/profile/admin.png";
 
 const Profile = () => {
   const [anchorEl2, setAnchorEl2] = useState(null);
   const [userRole, setUserRole] = useState("");
   const [userNameInitial, setUserNameInitial] = useState("");
   const [userGender, setUserGender] = useState(""); // Added state for user gender
+  const [residentFName, setResidentFName] = useState(""); // Added state for resident first name
+  const [residentLName, setResidentLName] = useState(""); // Added state for resident last name
 
   const handleClick2 = (event) => {
     setAnchorEl2(event.currentTarget);
@@ -26,6 +37,8 @@ const Profile = () => {
     const storedUserRole = localStorage.getItem("userRole");
     const storedUserName = localStorage.getItem("userN");
     const storedUserGender = localStorage.getItem("userGender"); // Retrieve user gender
+    const storedResidentFName = localStorage.getItem("resFirstName"); // Retrieve resident first name
+    const storedResidentLName = localStorage.getItem("resLastName"); // Retrieve resident last name
 
     if (storedUserRole) {
       setUserRole(storedUserRole);
@@ -38,6 +51,14 @@ const Profile = () => {
     if (storedUserGender) {
       setUserGender(storedUserGender);
     }
+
+    if (storedResidentFName) {
+      setResidentFName(storedResidentFName);
+    }
+
+    if (storedResidentLName) {
+      setResidentLName(storedResidentLName);
+    }
   }, []);
 
   const auth = userRole === "admin" ? useAdminAuth() : useResidentAuth();
@@ -45,6 +66,20 @@ const Profile = () => {
   const handleLogout = async () => {
     await auth.logout();
   };
+
+  // Set the image source based on user gender or default to admin image
+  const avatarSrc =
+    userGender === "Female"
+      ? doctorFemaleImage.src
+      : userGender === "Male"
+      ? doctorMaleImage.src
+      : adminImage.src;
+
+  // Determine the name to display based on whether it's a resident or admin
+  const displayName =
+    residentFName && residentLName
+      ? `${residentFName} ${residentLName}`
+      : "Admin";
 
   return (
     <Box>
@@ -63,18 +98,18 @@ const Profile = () => {
       >
         {/* Conditionally render the Avatar based on user gender */}
         <Avatar
-          src={
-            userGender === "Female"
-              ? doctorFemaleImage.src
-              : doctorMaleImage.src
-          }
-          alt="Doctor"
+          src={avatarSrc}
+          alt="Avatar"
           sx={{
             width: 35,
             height: 35,
             backgroundColor: "primary.main",
           }}
         />
+        {/* Display the user's name */}
+        <Typography variant="body1" sx={{ marginLeft: 1 }}>
+          {displayName}
+        </Typography>
       </IconButton>
       <Menu
         id="msgs-menu"
