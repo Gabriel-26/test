7078124316020874a38c7dde21cff512a49434f2
Gabriel Tejana <gabriel.tejana26@gmail.com>
2024-04-03 17:02:19 +0800

    another attempt to fix date in edit in medication

--- pages/utilities/patientroom/Medications.tsx
+++ pages/utilities/patientroom/Medications.tsx
@@ -157,13 +157,18 @@ const Medication = (props: any) => {
   const handleEdit = (medication) => {
     console.log("Selected Medication for Edit:", medication);
     setSelectedMedication(medication);
+    
+    // Format the date to match the DatePicker format
+    const formattedDate = moment(medication.patientMedicineDate).format("YYYY-MM-DD");
+    
     form.setFieldsValue({
       medicine_id: medication.medicine_name,
       medicine_frequency: medication.medicine_frequency,
-      patientMedicineDate: moment(medication.patientMedicineDate, "YYYY-MM-DD"), // Convert to moment object with correct format
+      patientMedicineDate: moment(formattedDate), // Convert to moment object
     });
     setEditModalVisible(true);
   };
+  
 
   const handleEditSubmit = async (values, selectedDate) => {
     try {
