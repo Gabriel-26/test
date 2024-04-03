b40d67ae2edce0a18b21e555f181bd2c77318489
Gabriel Tejana <gabriel.tejana26@gmail.com>
2024-04-03 15:12:05 +0800

    revert back

--- pages/utilities/floors/[floor_name].tsx
+++ pages/utilities/floors/[floor_name].tsx
@@ -635,8 +635,7 @@ const Rooms = () => {
 
           <Form.Item name="room_type" label="Room Type">
   <Select
-    value={editRoomType}
-    onChange={(value) => setEditRoomType(value)} // Allow the user to change the room type
+  
     placeholder="Select room type"
   >
     <Option value="Private">Private</Option>
